e3b9d169a1f10c75f6443fafed5d1407847d8627
Dudu <edu.chaos@gmail.com>
2015-12-02 13:53:02 -0200

    fix api url

--- src/main/resources/www/lib/loto-api.js
+++ src/main/resources/www/lib/loto-api.js
@@ -4,11 +4,13 @@ define(
 	function ($) {
         return {
             getLotofacil: function() {
-                return $.getJSON("http://127.0.0.1:9000/api/lotofacil");
+                //return $.getJSON("http://127.0.0.1:9000/api/lotofacil");
+                return $.getJSON("/api/lotofacil");
             },
 
             getLotofacilBets: function(concurso) {
-                return $.getJSON("http://127.0.0.1:9000/api/lotofacil/" + concurso + "/bets");
+                //return $.getJSON("http://127.0.0.1:9000/api/lotofacil/" + concurso + "/bets");
+                return $.getJSON("/api/lotofacil/" + concurso + "/bets");
             },
 
             getHello: function () {
